cb722b25d2248ed9135ddfff3e887094c5289064
JuanM04 <me@juanm04.com>
2019-06-05 16:53:29 -0300

    Password system (Endpoint)

---
+++ api/check-token.js
@@ -0,0 +1,23 @@
+require('dotenv').config()
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const { checkToken } = require('../utils')
+
+const app = express()
+module.exports = app
+
+app.use(bodyParser.json())
+
+app.post('*', (req, res) => {
+  if (req.body == null) return res.status(400).send({ error: 'no JSON object in the request' })
+  
+  const JSONRes = { token: checkToken(process.env.JWT_SECRET, req.body.token) }
+
+  res.set('Content-Type', 'application/json')
+  res.status(200).send(JSON.stringify(JSONRes, null, 4))
+})
+
+app.all('*', (req, res) => {
+  res.status(405).send({ error: 'only POST requests are accepted' })
+})
