d96cbadcb6015fc775814732cc5c30779f7376d1
JuanM04 <me@juanm04.com>
2019-06-05 16:56:04 -0300

    New index

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,15 @@
 import React from 'react'
+import Link from 'next/link'
+import { Button, Container } from 'shards-react'
 
 
 
 export default props => (
-  <h1>Login</h1>
+  <Container className="center index">
+    <h1>Veritas</h1>
+    <span>Creado por <a href="https://juanm04.com" noreferrer="true" noopener="true" target="_blank">JuanM04</a></span>
+    <Link href="/acceder" prefetch>
+      <Button>Acceder</Button>
+    </Link>
+  </Container>
 )
