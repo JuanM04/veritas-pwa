5fe4ecb30e1db0b6a20375221ff39357bbb72467
JuanM04 <me@juanm04.com>
2019-06-07 22:31:40 -0300

    Fixed rare bug

--- components/tabs.js
+++ components/tabs.js
@@ -1,25 +1,33 @@
 import React from 'react'
-import Link from 'next/link'
+import Router from 'next/router'
 import { Container, Col, Row } from 'shards-react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { tabs as TABS } from 'utils/metadata'
 
 
 
-export default props => (
-  <footer className="with-shadows">
-    <Container>
-      <Row>
-        {TABS.map((tab, i) => (
-          <Link key={i} href={tab.path} prefetch={props.current === i}>
-            <Col key={i} className={'tab' + (props.current === i ? ' current' : '')}>
+export default props => {
+  const redirect = i => {
+    if(props.current !== i) Router.push(TABS[i].path)
+  }
+
+  return(
+    <footer className="with-shadows">
+      <Container>
+        <Row>
+          {TABS.map((tab, i) => (
+            <Col
+              key={i}
+              className={'tab' + (props.current === i ? ' current' : '')}
+              onClick={() => redirect(i)}
+            >
               <FontAwesomeIcon icon={tab.icon} size="lg" />
               <br/>
               <span>{tab.name}</span>
             </Col>
-          </Link>
-        ))}
-      </Row>
-    </Container>
-  </footer>
-)
+          ))}
+        </Row>
+      </Container>
+    </footer>
+  )
+}
